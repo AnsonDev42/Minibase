d6a61039da47dcbeed8ebe05a7dc892059c42d9c
ans0n <tmmanson77@gmail.com>
2023-02-04 23:56:32 +0000

    add dic:{var1:[1,2] ..} for preparing removing
    -next step: for each var in dic, iterate the list and build the possible reduction list

--- src/main/java/ed/inf/adbs/minibase/CQMinimizer.java
+++ src/main/java/ed/inf/adbs/minibase/CQMinimizer.java
@@ -1,13 +1,10 @@
 package ed.inf.adbs.minibase;
 
-import ed.inf.adbs.minibase.base.Atom;
-import ed.inf.adbs.minibase.base.Query;
-import ed.inf.adbs.minibase.base.Head;
-import ed.inf.adbs.minibase.base.RelationalAtom;
+import ed.inf.adbs.minibase.base.*;
 import ed.inf.adbs.minibase.parser.QueryParser;
 
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 /**
  *
@@ -42,15 +39,65 @@ public class CQMinimizer {
         // TODO: add your implementation
         try {
 //            Query query = QueryParser.parse(Paths.get(filename));
-             Query query = QueryParser.parse("Q(x, y) :- R(x, z), S(y, z, w)");
+             Query query = QueryParser.parse("Q(x, y) :- R(x, z), S(y, z, w), S(y,z,k)");
             // Query query = QueryParser.parse("Q(x) :- R(x, 'z'), S(4, z, w)");
 
             System.out.println("Entire query: " + query);
             Head head = query.getHead();
             System.out.println("Head: " + head);
-            List<RelationalAtom> body = query.getBody();
+            List<Atom> body = query.getBody();
             System.out.println("Body: " + body);
-            System.out.println("Body0: " + body[0]);
+
+            // create a set for the restricted variable names
+            List<Variable> restricted_var_list = head.getVariables();
+            Set<String> restricted_var_set = new HashSet<>();
+            for (Variable var : restricted_var_list) {
+                restricted_var_set.add(var.getName());
+            }
+
+            // create an empty set for the allowed variables
+            HashMap<String,ArrayList> allowed_var_set = new HashMap<>();
+
+
+            //  add potential variables to the allowed variables list
+            int i = 0;
+            for (Atom atom : body) {
+                if (atom instanceof RelationalAtom) {
+                    RelationalAtom relationalAtom = (RelationalAtom) atom;
+                    List<Term> terms = relationalAtom.getTerms();
+                    for (Term term : terms) {
+                        if (term instanceof Variable && !restricted_var_set.contains(term.toString())){
+                            if (!allowed_var_set.containsKey(term.toString())) {
+                                // add var to set with empty index list
+                                allowed_var_set.put(term.toString(), new ArrayList<Integer>());
+                            }
+                            // add index to index list
+                            allowed_var_set.get(term.toString()).add(i);
+                        }
+                    }
+                    i++;
+                }
+            }
+            System.out.println("Allowed variables: " + allowed_var_set);
+
+            // remove the allowed variables from the body if it can be removed
+            String atom_to_remove = "w";
+            ArrayList<Integer> removing_index =  new ArrayList<Integer>();
+            for (Atom atom : body) {
+                if (atom instanceof RelationalAtom) {
+                    RelationalAtom relationalAtom = (RelationalAtom) atom;
+                    List<Term> terms = relationalAtom.getTerms();
+                    // iterate through the terms by the removing_index
+                    for (int index : removing_index) {
+                        if (terms.get(index) instanceof Variable && terms.get(index).toString().equals(atom_to_remove)) {
+                            // remove the atom
+                            body.remove(atom);
+                            break;
+                        }
+                    }
+                }
+            }
+
         }
         catch (Exception e)
         {
---
+++ src/main/java/ed/inf/adbs/minibase/base/ReduceFromComparison.java
@@ -0,0 +1,9 @@
+package ed.inf.adbs.minibase.base;
+
+public class ReduceFromComparison{
+
+    public ReduceFromComparison(Term term1, Term term2, String tobeReduced) {
+
+
+    }
+}
